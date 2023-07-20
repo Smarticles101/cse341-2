edcbe8bb93b166edd806139e335f030fb2af93c3
Logan Stucki <loganstucki@gmail.com>
2023-07-19 20:59:39 -0600

    add user routes

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,104 @@ const { ObjectId } = require("mongodb");
 const db = require("../db/database");
 const passport = require("passport");
 
-module.exports = {
+const getUsers = (req, res) => {
+  const session_user = req.session.passport.user;
+  db.getDB()
+    .db()
+    .collection("users")
+    .find()
+    .toArray()
+    .then((data) => {
+      res.status(200).send(
+        data.map((user) => {
+          // only return non-sensitive information
 
+          if (user._id === session_user.id) {
+            return user;
+          }
+          return {
+            _id: user._id,
+            name: user.name,
+            avatar: user.avatar,
+          };
+        })
+      );
+    });
+};
+
+const getUserById = (req, res) => {
+  const id = new ObjectId(req.params.id);
+  const session_user = req.session.passport.user;
+
+  db.getDB()
+    .db()
+    .collection("users")
+    .find({ _id: id })
+    .toArray()
+    .then((data) => {
+      res.status(200).send(
+        data.map((user) => {
+          if (user._id === session_user.id) {
+            return user;
+          }
+          return {
+            _id: user._id,
+            name: user.name,
+            avatar: user.avatar,
+          };
+        })[0]
+      );
+    });
+};
+
+const modifyUser = (req, res) => {
+  const id = new ObjectId(req.params.id);
+  const session_user = req.session.passport.user;
+
+  if (id !== session_user.id) {
+    res.status(401).send("Unauthorized");
+  }
+
+  const user = {
+    name: req.body.name,
+    avatar: req.body.avatar,
+  };
+
+  if (user.name.length < 1) {
+    res.status(400).send("Name cannot be empty");
+  } else if (user.name.length > 255) {
+    res.status(400).send("Name cannot be longer than 255 characters");
+  }
+
+  db.getDB()
+    .db()
+    .collection("users")
+    .updateOne({ _id: id }, { $set: user })
+    .then((result) => {
+      res.status(201).send(result);
+    });
+};
+
+const deleteUser = (req, res) => {
+  const id = new ObjectId(req.params.id);
+  const session_user = req.session.passport.user;
+
+  if (id !== session_user.id) {
+    res.status(401).send("Unauthorized");
+  }
+
+  db.getDB()
+    .db()
+    .collection("users")
+    .deleteOne({ _id: id })
+    .then((result) => {
+      res.status(201).send(result);
+    });
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  modifyUser,
+  deleteUser,
 };
--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,12 @@ const router = express.Router();
 
 const usersController = require("../controllers/users");
 
+router.get("/", usersController.getUsers);
+
+router.get("/:id", usersController.getUserById);
+
+router.put("/:id", usersController.modifyUser);
+
+router.delete("/:id", usersController.deleteUser);
+
 module.exports = router;
