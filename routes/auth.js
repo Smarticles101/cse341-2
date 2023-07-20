6f2bffe1cf595fc219513bd1d6a799a3d3f12498
Logan Stucki <loganstucki@gmail.com>
2023-07-19 21:01:13 -0600

    add logout route

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,9 @@ routes.get("/auth", passport.authenticate("google", { scope: ["profile", "email"
 routes.get("/auth_callback", passport.authenticate("google", { failureRedirect: "/auth" }), (req, res) => {
     res.sendStatus(200);
 });
+routes.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/");
+});
 
 module.exports = routes;
